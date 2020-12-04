faa3da7afe0cb29073753500955770dd26c958b3
Vijay Selvaraj <vijay@Vijays-MacBook-Pro.local>
2020-12-03 22:26:51 -0500

    added sorting

--- RecipeFinder/indexer.py
+++ RecipeFinder/indexer.py
@@ -7,7 +7,7 @@ from java.nio.file import Paths
 from org.apache.lucene.store import MMapDirectory
 from org.apache.lucene.analysis.en import EnglishAnalyzer
 from org.apache.lucene.document import Document, Field, FieldType
-from org.apache.lucene.index import IndexWriter, IndexWriterConfig, IndexOptions
+from org.apache.lucene.index import IndexWriter, IndexWriterConfig, IndexOptions, DocValuesType
 
 if __name__ == "__main__":
     lucene.initVM()
@@ -23,6 +23,11 @@ if __name__ == "__main__":
     t2.setStored(True)
     t2.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS)
 
+    t3 = FieldType()
+    t3.setStored(True)
+    t3.setDocValuesType(DocValuesType.NUMERIC)
+    t3.setIndexOptions(IndexOptions.NONE)
+
     print(f"{writer.numRamDocs()} docs in index")
 
     for df in pd.read_csv('dataset/recipe.csv', chunksize=5000, iterator=True):
@@ -32,11 +37,11 @@ if __name__ == "__main__":
 
             doc = Document()
 
-            doc.add(Field("id", row["recipe_id"], t1))
+            doc.add(Field("id", row["recipe_id"], t3))
             doc.add(Field("name", row["recipe_name"], t1))
-            doc.add(Field("avg_rating", row["aver_rate"], t1))
+            doc.add(Field("avg_rating", row["aver_rate"], t3))
             doc.add(Field("image", row["image_url"], t1))
-            doc.add(Field("total_reviews", row["review_nums"], t1))
+            doc.add(Field("total_reviews", row["review_nums"], t3))
 
             ingredients = row["ingredients"].split("^")
             for ingredient in ingredients:
@@ -44,11 +49,21 @@ if __name__ == "__main__":
 
             parsed = ast.literal_eval(row["cooking_directions"])
             directions = parsed["directions"].split("\n")
-            for direction in directions:
+            total_time = '0'
+            for idx, direction in enumerate(directions):
                 doc.add(Field("directions", direction, t1))
+                if direction.lower() == 'ready in' and idx <= 4:
+                    total_time = directions[idx+1]
+
+            total_time = total_time.replace('d', '* 24 * 60 +')\
+                .replace('h', '* 60 +').replace('m', '').strip()
+            total_time = total_time[:-1] if total_time.endswith('+')\
+                else total_time
+            total_time = eval(total_time)
+            doc.add(Field("total_time", total_time, t3))
 
             nutrition = ast.literal_eval(row["nutritions"])
-            doc.add(Field("calories", nutrition["calories"]["amount"], t1))
+            doc.add(Field("calories", nutrition["calories"]["amount"], t3))
 
             nutrition = json.dumps(nutrition)
             doc.add(Field("nutrition", nutrition, t1))
--- RecipeFinder/retriever.py
+++ RecipeFinder/retriever.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from java.nio.file import Paths
 from org.apache.lucene.store import MMapDirectory
 from org.apache.lucene.index import DirectoryReader
-from org.apache.lucene.search import IndexSearcher
+from org.apache.lucene.search import IndexSearcher, Sort, SortField
 from org.apache.lucene.analysis.en import EnglishAnalyzer
 from org.apache.lucene.queryparser.classic import QueryParser
 
@@ -16,6 +16,8 @@ def convert_to_json(doc):
         'id': doc.get('id'),
         'name': doc.get('name'),
         'image': doc.get('image'),
+        'avg_rating': doc.get('avg_rating'),
+        'total_reviews': doc.get('total_reviews'),
         'ingredients': convert_to_list(doc, "ingredients"),
         'directions': convert_to_list(doc, "directions"),
         'nutrition': doc.get('nutrition')
@@ -35,13 +37,17 @@ if __name__ == "__main__":
     searchTerm = input("Enter query: ")
     query = queryParser.parse(searchTerm)
 
+    sortFields = [SortField("total_reviews", SortField.Type.FLOAT, True),
+                  SortField("avg_rating", SortField.Type.FLOAT, True),
+                  SortField.FIELD_SCORE]
+    sort = Sort(sortFields)
+
     print(f"Current Time = {datetime.now()}")
-    hits = searcher.search(query, 20)
+    hits = searcher.search(query, 20, sort)
     print(f"Current Time = {datetime.now()}")
 
     print(f"Found {hits.totalHits.value} document(s) that matched query '{query}':")
     for hit in hits.scoreDocs:
-        print(hit.score, hit.doc, hit.toString())
         doc = searcher.doc(hit.doc)
         print(convert_to_json(doc))
 
